f48681972c94e3af7c71b53e3f6c33d1a138e859
Hernan Seco <hseco@sw.co>
2021-06-04 23:36:46 -0500

    commit consulta de usuarios

--- servers/Usuarios.php
+++ servers/Usuarios.php
@@ -1,21 +1,39 @@
 <?php
 require_once '../config/databaseconnect.php';
 
-
-if($_REQUEST['opcion'] == 'registro') { //Registro Usuarios
-
-    $sql="
+switch ($_REQUEST['opcion']) {
+    case "registro":
+        $sql="
         insert into usuarios(nombres, apellidos, documento, cargo_id) 
         values('".$_REQUEST['obj_user']['nombres']."','".$_REQUEST['obj_user']['apellidos']."','".$_REQUEST['obj_user']['documento']."',".$_REQUEST['obj_user']['cargo'].")";
 
-    $consulta=mysqli_query($conexion, $sql);
+        $consulta=mysqli_query($conexion, $sql);
 
-    if ($consulta)
-        echo 'true';
-    else
-        echo 'false';
+        if ($consulta)
+            echo 'true';
+        else
+            echo 'false';
+        break;
+    case "modificar":
+        echo "i es una barra";
+        break;
+    case "consulta":
+        echo "i es un pastel";
+        break;
+    case "usuarios":
+        $sql="select u.id_usuario, u.nombres, u.apellidos, tc.nombre_cargo from usuarios u inner join tipo_cargo tc on tc.id = u.cargo_id";
 
+        $consulta=mysqli_query($conexion, $sql);
+
+        $datos = [];
+        while ($fila = mysqli_fetch_assoc($consulta)) {
+            $datos[] = $fila;
+        }
+
+        echo json_encode($datos);
+        break;
 }
+
 
 if($_REQUEST['opcion'] == 'modificar') { //Modificar Usuarios
 
@@ -38,4 +56,7 @@ if($_REQUEST['opcion'] == 'consulta') { //consultar Usuario
 
     return json_encode($row);
 }
+
+
+
 
