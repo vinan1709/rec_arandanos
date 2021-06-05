e6fbfae40aee9409dd77da2034202c4404a08bbd
Laura Rojas <vinan1709@gmail.com>
2021-06-04 21:03:53 -0500

    js registro

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,27 @@
 $( document ).ready(function() {
+
     console.log( "ready!" );
-    jQuery(".adduser").click({
-        
-    })
+    $(".adduser").click(function() {debugger
+        var name = $("#name").val();
+        var apellidos = $("#apellidos").val();
+        var documento = $("#documento").val();
+        var cargo = $(".form-select").val();
+
+        var obj_user = {
+            name : name,
+            apellidos : apellidos,
+            documento : documento,
+            cargo : cargo,
+        }
+        console.log(obj_user);
+        jQuery.ajax({
+            
+            method: "POST",
+            url: "../servers/Usuarios.php",
+            data: {obj_user, opcion:"registro"}
+        })
+        .done(function(msg) {
+        });
+      });
+
 });
